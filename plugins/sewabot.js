beac3ae5221deea02c7dab30c8049f91c1e5ca46
Ramabotz9 <108044415+Ramabotz9@users.noreply.github.com>
2022-06-29 19:54:19 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -13,15 +13,15 @@ let handler = async (m, { conn }) => {
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
 1. Grup / 30 Hari
-Rp. 15.000 Dana
+Rp. 15.000 Gopay
 Rp. 15.000 Pulsa
 
 2. Premium / 30 Hari
-Rp. 10.000 Dana
+Rp. 10.000 Gopay
 Rp. 10.000 Pulsa
 
 3. Premium + Grup / 30 Hari
-Rp. 20.000 Dana
+Rp. 20.000 Gopay
 Rp. 20.000 Pulsa
 
 wa.me/${owner[0]}
